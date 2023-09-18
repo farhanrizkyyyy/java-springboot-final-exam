4a7e81f50de1c148384fe16256dcc6d81e46f0d0
Farhan Rizky <farhanrizkyyyy@gmail.com>
2023-09-18 23:15:15 +0700

    add category seeder

--- src/main/java/com/example/finalexam/models/Category.java
+++ src/main/java/com/example/finalexam/models/Category.java
@@ -1,8 +1,9 @@
 package com.example.finalexam.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,7 +17,8 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Getter @Setter @AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "categories")
 public class Category {
     @Id
@@ -30,14 +32,21 @@ public class Category {
     private String name;
 
     @OneToMany(mappedBy = "category")
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
 
+    @CreationTimestamp
     @Column(name = "created_at")
     private Date createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
 
     @Column(name = "deleted_at")
     private Date deletedAt;
+
+    public Category(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 }
--- src/main/java/com/example/finalexam/repositories/CategoryRepository.java
+++ src/main/java/com/example/finalexam/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.example.finalexam.repositories;
 import com.example.finalexam.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByDeletedAtIsNullOrderByNameAsc();
 }
